844ef12eaa5071d3b2ee0835ae1f708f60565768
UJWAL KUMAR <89293303+ujwxl23@users.noreply.github.com>
2023-01-01 14:10:06 +0530

    Updated
    Run successfully.

--- module1/src/bin/func2.rs
+++ module1/src/bin/func2.rs
@@ -8,4 +8,17 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn main() {
+  let n1 = 2;
+  let n2 = 3;
+  let s = add_func(n1,n2);
+  display_func(s);
+}
+
+fn add_func(x1:i32,x2:i32)->i32{
+  return x1+x2;
+}
+
+fn display_func(q:i32){
+  println!("{:?}",q);
+}
