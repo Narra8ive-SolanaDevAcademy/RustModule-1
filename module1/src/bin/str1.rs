9958736388008e5338e8ee4d47e037d9fe1782de
codescalper <msfunbook@gmail.com>
2023-10-19 18:13:12 +0530

    Fixed error in str1.rs

--- module1/src/bin/str1.rs
+++ module1/src/bin/str1.rs
@@ -1,24 +1,15 @@
 // Fix all errors without adding newline
 fn main() {
-
-    let mut  s =  String::from("hello");
+    let mut s = String::from("hello");
     s.push(',');
     s.push_str("world");
 
-  
-    
-
-
-    s += '!';
-
-   
-
+    s += "!";
+    println!("{:?}", s)
 }
 
-
-
-
-
+// push is useed to add single character in a string
+// push_str() is used to add slice of string in the end of the string
 
 /*
  Hint:  read the difference between push and push_str
